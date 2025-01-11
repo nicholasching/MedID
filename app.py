3e15fa586fe332a2120c2fd9d353ae15450d0a9a
Nicholas C <nicholaslching@gmail.com>
2025-01-11 14:28:19 -0500

    Implemented Person Recognition

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, Response, render_template
 import cv2
 import time
+import numpy as np
 
 app = Flask(__name__)
 
@@ -12,12 +13,102 @@ if not video_source.isOpened():
     print("Error: Could not open video source.")
     exit()
 
+# Adjustment Tolerances
+tolerance = 0.8
+
+#Import the neccesary libraries
+import numpy as np
+import argparse
+import cv2 
+
+# construct the argument parse 
+parser = argparse.ArgumentParser(
+    description='Script to run MobileNet-SSD object detection network ')
+parser.add_argument("--video", help="path to video file. If empty, camera's stream will be used")
+parser.add_argument("--prototxt", default="MobileNetSSD_deploy.prototxt",
+                                  help='Path to text network file: '
+                                       'MobileNetSSD_deploy.prototxt for Caffe model or '
+                                       )
+parser.add_argument("--weights", default="MobileNetSSD_deploy.caffemodel",
+                                 help='Path to weights: '
+                                      'MobileNetSSD_deploy.caffemodel for Caffe model or '
+                                      )
+parser.add_argument("--thr", default=0.2, type=float, help="confidence threshold to filter out weak detections")
+args = parser.parse_args()
+
+# Labels of Network.
+classNames = { 0: 'background',
+    1: 'aeroplane', 2: 'bicycle', 3: 'bird', 4: 'boat',
+    5: 'bottle', 6: 'bus', 7: 'car', 8: 'cat', 9: 'chair',
+    10: 'cow', 11: 'diningtable', 12: 'dog', 13: 'horse',
+    14: 'motorbike', 15: 'person', 16: 'pottedplant',
+    17: 'sheep', 18: 'sofa', 19: 'train', 20: 'tvmonitor' }
+
+#Load the Caffe model 
+net = cv2.dnn.readNetFromCaffe(args.prototxt, args.weights)
+
+def process_frames(frame):
+    # Adding Bounding box
+    frame_resized = cv2.resize(frame,(300,300)) # resize frame for prediction
+
+    # MobileNet requires fixed dimensions for input image(s)
+    # so we have to ensure that it is resized to 300x300 pixels.
+    # set a scale factor to image because network the objects has differents size. 
+    # We perform a mean subtraction (127.5, 127.5, 127.5) to normalize the input;
+    # after executing this command our "blob" now has the shape:
+    # (1, 3, 300, 300)
+    blob = cv2.dnn.blobFromImage(frame_resized, 0.007843, (300, 300), (127.5, 127.5, 127.5), False)
+    #Set to network the input blob 
+    net.setInput(blob)
+    #Prediction of network
+    detections = net.forward()
+
+    #Size of frame resize (300x300)
+    cols = frame_resized.shape[1] 
+    rows = frame_resized.shape[0]
+
+    #For get the class and location of object detected, 
+    # There is a fix index for class, location and confidence
+    # value in @detections array .
+    for i in range(detections.shape[2]):
+        confidence = detections[0, 0, i, 2] #Confidence of prediction 
+        if confidence > args.thr: # Filter prediction 
+            class_id = int(detections[0, 0, i, 1]) # Class label
+
+            # Object location 
+            xLeftBottom = int(detections[0, 0, i, 3] * cols) 
+            yLeftBottom = int(detections[0, 0, i, 4] * rows)
+            xRightTop   = int(detections[0, 0, i, 5] * cols)
+            yRightTop   = int(detections[0, 0, i, 6] * rows)
+            
+            # Factor for scale to original size of frame
+            heightFactor = frame.shape[0]/300.0  
+            widthFactor = frame.shape[1]/300.0 
+            # Scale object detection to frame
+            xLeftBottom = int(widthFactor * xLeftBottom) 
+            yLeftBottom = int(heightFactor * yLeftBottom)
+            xRightTop   = int(widthFactor * xRightTop)
+            yRightTop   = int(heightFactor * yRightTop)
+            # Draw location of object  
+            cv2.rectangle(frame, (xLeftBottom, yLeftBottom), (xRightTop, yRightTop), (0, 255, 0))
+
+            # Print label and confidence of prediction
+            if class_id in classNames:
+                label = classNames[class_id] + ": " + str(confidence)
+                if(classNames[class_id] == "person" and confidence > tolerance):
+                    cv2.putText(frame, "Person Detected", (frame.shape[1] - 200, frame.shape[0] - 25), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255))
+                print(label) #print class and confidence
+                return label
+
 def generate_frames():
     while True:
         success, frame = video_source.read()
         if not success:
             break
-        # Optionnal: Process frame if needed here
+
+        # Processing Frames
+        label = process_frames(frame)
+
         # Convert frame to JPEG format
         _, buffer = cv2.imencode('.jpg', frame) 
         # Yield the JPEG image in bytes
